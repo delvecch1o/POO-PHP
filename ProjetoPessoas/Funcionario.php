d736d9dbad80c7ad88a9dd575b939165982a7a32
delvecch1o <rodrigopereirapinheiro@gmail.com>
2021-04-06 22:49:37 -0300

    Herança

---
+++ ProjetoPessoas/Funcionario.php
@@ -0,0 +1,28 @@
+<?php
+require_once 'Pessoa.php';
+class Funcionario extends Pessoa {
+    private $setor;
+    private $trabalhando;
+    
+    public function mudarTrabalho(){
+        $this->trabalhando = !$this->trabalhando; //trabalhando recebe NÃO trabalhando um inversor.
+    }
+    public function getSetor() {
+        return $this->setor;
+    }
+
+    public function getTrabalhando() {
+        return $this->trabalhando;
+    }
+
+    public function setSetor($setor) {
+        $this->setor = $setor;
+    }
+
+    public function setTrabalhando($trabalhando) {
+        $this->trabalhando = $trabalhando;
+    }
+
+
+    
+}
