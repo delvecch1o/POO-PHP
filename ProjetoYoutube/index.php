68776680c3091e50e8f4df439d3368c90597e84a
delvecch1o <rodrigopereirapinheiro@gmail.com>
2021-04-09 20:01:47 -0300

    agregação

--- ProjetoYoutube/index.php
+++ ProjetoYoutube/index.php
@@ -12,6 +12,7 @@
         require_once 'Video.php';
         require_once 'Pessoa.php';
         require_once 'JovemAprendiz.php';
+        require_once 'Vizualizacao.php';
         
         $v[0] = new Video("Aula 1 de POO");
         $v[1] = new Video("Aula 12 de PHP");
@@ -24,6 +25,18 @@
         
         $j[1] = new JovemAprendiz("Simara", 18 , "F", "suave");
         print_r($j[1]);
+        
+        
+        $vis[0] = new Vizualizacao($j[0],$v[2]); //Rian assiste HTML5
+        $vis[1] = new Vizualizacao($j[0],$v[1]);
+        
+        $vis[0]->avaliar();
+        $vis[1]->avaliarPorc(85);
+        
+        
+        
+        print_r($vis);
+                
         
         
         ?>
